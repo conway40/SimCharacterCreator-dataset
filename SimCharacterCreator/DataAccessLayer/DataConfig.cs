e139cbde19c59c907e17f46389287a3e2ad0ab7b
conway40 <conway40@mail.nmc.edu>
2019-12-08 15:04:13 -0500

    Published remotely on AWS. Removed login from navigation. Added default image.

--- SimCharacterCreator/DataAccessLayer/DataConfig.cs
+++ SimCharacterCreator/DataAccessLayer/DataConfig.cs
@@ -14,7 +14,7 @@ namespace SimCharacterCreator.DataAccessLayer
         //public static DataType dataType = DataType.XML;
         public static SeedDataType dataType = SeedDataType.JSON;
 
-        public static string DataPathJson => @"C:\Users\Caitlin\Desktop\SimCharacterCreator-dataset\SimCharacterCreator\DataAccessLayer\simdata.json";
+        public static string DataPathJson => @"C:\www\sim-character-creator\DataAccessLayer\simdata.json";
         //public static string DataPathXml => @"DataAccessLayer\.xml";
     }
 }
